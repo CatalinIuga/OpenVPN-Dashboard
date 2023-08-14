aecfffb598287ee34b07da114a59ab90579c3343
CatalinIuga <cataliniuga34@gmail.com>
2023-08-14 16:49:32 +0300

    working demo needs minor adjustments

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 
+import os
+
 from flask import (Flask, jsonify, redirect, render_template, request,
                    send_file, session)
 
-from script import get_clients, get_config, revoke_client
-
-# For UNIX systems only
-# if os.getuid() != 0:
-#     print("Please run using SUDO!")
-#     exit(1)
+from script import create_client, get_clients, get_config, revoke_client
 
 app = Flask(__name__)
-# change this to an environment variable
 app.secret_key = 'mayb3_1ts_4_s3cr3t_k3y'
-
-# change this to an environment variable
 PASSWORD = 'eef2decbc5d8f20aaeaf63e2ac8371aee5d0b4733770aef60cfa116a88b5f28c'
 
 
@@ -50,18 +44,7 @@ def logout():
 def clients():
     if not session.get('logged_in'):
         return redirect('/login')
-    # clients_arr = get_clients()
-    clients_arr = [
-        {"bytes_recv": 0, "bytes_sent": 0, "connected": False, "connected_since": None,
-            "last_ref": None, "name": "ubu", "real_ip": None, "virtual_ip": None},
-        {"bytes_recv": "154143", "bytes_sent": "152792", "connected": True, "connected_since": "2023-08-09 04:33:54", "last_ref": "2023-08-09 10:26:03",
-         "name": "lubuntu", "real_ip": "192.168.1.6:59567", "virtual_ip": "10.8.0.2"},
-        {"bytes_recv": "77863", "bytes_sent": "75930", "connected": True, "connected_since": "2023-08-09 08:14:51",
-            "last_ref": "2023-08-09 10:29:33", "name": "lubuntu2", "real_ip": "192.168.1.4:58613", "virtual_ip": "10.8.0.3"},
-        {"bytes_recv": 0, "bytes_sent": 0, "connected": False, "connected_since": None,
-            "last_ref": None, "name": "test", "real_ip": None, "virtual_ip": None}
-    ]
-    # clients_arr = []
+    clients_arr = get_clients()
 
     return jsonify(clients_arr)
 
@@ -78,7 +61,7 @@ def config(name):
         return send_file(result, as_attachment=True)
 
 
-@app.route("/api/clients/<name>/revoke", methods=['GET'])
+@app.route("/api/clients/<name>/revoke", methods=['POST'])
 def revoke(name):
     if not session.get('logged_in'):
         return redirect('/login')
@@ -86,19 +69,29 @@ def revoke(name):
     result = revoke_client(name)
     if result == None:
         return jsonify({"status": "error", "message": "Client not found"})
+    if result == False:
+        return jsonify({"status": "error", "message": "Client coudn't be revoked. Please try again."})
     else:
         return jsonify({"status": "success", "message": "Client revoked successfully"})
 
 
-@app.route("/api/clients/<name>/create", methods=['GET'])
+@app.route("/api/clients/<name>/create", methods=['POST'])
 def create(name):
     if not session.get('logged_in'):
         return redirect('/login')
 
-    # result = create_client(name)
-    result = None
+    result = create_client(name)
     if result == None:
         return jsonify({"status": "error", "message": "Client already exists"})
+    elif result == False:
+        return jsonify({"status": "error", "message": "Cant add client at the moment. Please try again."})
     else:
-        # dont forget to refresh the page on frontend when this is called
         return jsonify({"status": "success", "message": "Client created successfully"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    if os.name == 'posix':
+        if os.getuid() != 0:
+            print("Please run using SUDO!")
+            exit(1)
--- script.py
+++ script.py
@@ -3,8 +3,11 @@ import os
 import re
 import subprocess
 
+transfer_speeds = {}
+
 
 def get_clients():
+    global transfer_speeds
     command = "cat /etc/openvpn/easy-rsa/pki/index.txt | grep ^V | awk -F \"/\" '{print $2}' | sed 's/CN=//'"
     certificates = subprocess.Popen(
         command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -21,10 +24,9 @@ def get_clients():
             "virtual_ip": None,
             "last_ref": None
         }
-        for name in names
+        for name in names if 'server' not in name
     ]
 
-    # print(client_list)
     process = subprocess.Popen(["timeout", "0.1s", "tail", "-f", "/var/log/openvpn/status.log"],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stderr_output = process.stderr.read().decode('utf-8')
@@ -34,30 +36,24 @@ def get_clients():
     else:
         stdout_output = process.stdout.read().decode('utf-8')
 
-    # Use the provided regex patterns
     client_info_pattern = r'([a-zA-Z0-9_]*),([0-9.:]*),([0-9]*),([0-9]*),([0-9 -:]*)$'
-    routing_info_pattern = r'([0-9.:]*),([a-zA-Z0-9_]*),([0-9.:]*),([0-9 -:]*)$'
-
-    # Find all matches of the patterns in the output
     client_info_matches = re.findall(
         client_info_pattern, stdout_output, re.MULTILINE)
-    routing_info_matches = re.findall(
-        routing_info_pattern, stdout_output, re.MULTILINE)
 
-    # Process client information matches
     for match in client_info_matches:
         common_name, real_address, bytes_received, bytes_sent, connected_since = match
         for client in client_list:
-            if 'server' in client["name"]:
-                continue
             if client["name"] == common_name:
                 client["connected"] = True
                 client["real_ip"] = real_address
-                client["bytes_recv"] = bytes_received
-                client["bytes_sent"] = bytes_sent
+                client["bytes_recv"] = int(bytes_received) / 1000
+                client["bytes_sent"] = int(bytes_sent) / 1000
                 client["connected_since"] = connected_since
 
-    # Process routing information matches
+    routing_info_pattern = r'([0-9.:]*),([a-zA-Z0-9_]*),([0-9.:]*),([0-9 -:]*)$'
+    routing_info_matches = re.findall(
+        routing_info_pattern, stdout_output, re.MULTILINE)
+
     for match in routing_info_matches:
         virtual_address, common_name, real_address, last_ref = match
         for client in client_list:
@@ -65,18 +61,13 @@ def get_clients():
                 client["virtual_ip"] = virtual_address
                 client["last_ref"] = last_ref
 
-    # Print the extracted client information
     return client_list
-
-# return the path of the client config file
-# the files then gets sent as an attachment to the client
 
 
 def get_config(name):
     if name == None:
         return None
 
-    # sanitize the name from the client
     for char in ['/', '\\', ':', '*', '?', '"', '<', '>', '|', ' ', "."]:
         name = name.replace(char, '')
 
@@ -84,17 +75,53 @@ def get_config(name):
     username = os.getlogin()
     path = "/home/" + username + "/" + name + ".ovpn"
 
-    # check if the file exists
     if os.path.isfile(path):
         return path
     else:
         return None
-    
+
 
 def revoke_client(name):
-    if name == None:
+    cmd = 'tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | cut -d \'=\' -f 2'
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    clients = process.stdout.read().decode('utf-8').split('\n')[:-1]
+
+    if name not in clients:
         return None
 
-    # sanitize the name from the client
-    for char in ['/', '\\', ':', '*', '?', '"', '<', '>', '|', ' ', "."]:
-        name = name.replace(char, '')
+    else:
+        index = clients.index(name) + 1
+
+    username = os.getlogin()
+    cmd = "/home/" + username + "/openvpn-install.sh"
+    process = subprocess.Popen(
+        cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = process.communicate(
+        input=b"2\n" + str(index).encode('utf-8') + b"\n")[0].decode('utf-8')
+    if "Revocation was successful" in result:
+        return True
+    return False
+
+
+def create_client(name):
+    command = "cat /etc/openvpn/easy-rsa/pki/index.txt | grep ^V | awk -F \"/\" '{print $2}' | sed 's/CN=//'"
+    certificates = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    names = certificates.stdout.read().decode('utf-8').split('\n')[:-1]
+    if name in names:
+        return None
+
+    username = os.getlogin()
+
+    cmd = "/home/" + username + "/openvpn-install.sh"
+    process = subprocess.Popen(
+        cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    result = process.communicate(
+        input=b"1\n" + name.encode('utf-8') + b"\n1\n")[0].decode('utf-8')
+
+    if "Download the .ovpn" in result:
+        return True
+    return False
